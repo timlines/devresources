99177b096b460b85b9ae740ebec534dc03df4984
Tim Lines <timlines89@gmail.com>
2025-02-17 15:28:24 -0600

    328 push

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import { PROJECT_ID, DATABASE_ID, COLLECTION_ID } from './keys.js';
 
-import { Client, Databases, ID } from "appwrite";
+import { Client, Databases, ID, Query } from "appwrite";
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
@@ -9,13 +9,23 @@ const client = new Client()
 
 const databases = new Databases(client);
 
-let myVar = await databases.listDocuments(
+let myQuery = await databases.listDocuments(
   DATABASE_ID,
   COLLECTION_ID,
   [
     Query.equal('category', ['Learning Resources'])
   ]
 );
+
+
+// Add all the resources to the results page
+async function addResourcesToDom(){
+  document.querySelector('ul').innerHTML = '<li>1</li>'
+  
+
+}
+
+
 
 console.log(myVar);
 
