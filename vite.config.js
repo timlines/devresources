8e5d2d649a4b327bd70f1e591780a0caa479f532
Tim Lines <timlines89@gmail.com>
2025-02-17 22:44:39 -0600

    Changing the root structure for multipage.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+  export default defineConfig({
     build: {
-      target: 'esnext' //browsers can handle the latest ES features
-    }
+        target: 'esnext', //browsers can handle the latest ES features
+
+        rollupOptions: {
+            input: {
+            main: resolve(__dirname, 'index.html'),
+            results: resolve(__dirname, 'results/index.html'),
+            },
+      },
+    },
   })
